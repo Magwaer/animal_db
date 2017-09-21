0ccb9044eb51454290eff00564041e5ad127fd69
Buzco Stanislav <magwaer@gmail.com>
2017-09-21 02:11:47 -0400

    angular material implemented

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+?>
+<html>
+<head>
+
+    
+</head>
+<body ng-app="BlankApp">
+this is demoxs
+
+    <script src="node_modules/angular/angular.min.js"></script>
+    <script src="node_modules/angular-material/angular-material.min.js"></script>
+    <script src="node_modules/angular-animate/angular-animate.min.js"></script>
+    <script src="node_modules/angular-aria/angular-aria.min.js"></script>
+    <script src="node_modules/angular-messages/angular-messages.min.js"></script>
+    <script src="node_modules/angular-material/angular-material.min.js"></script>
+    
+    <script src="static/js/app.js"></script>
+</body>
+</html>
